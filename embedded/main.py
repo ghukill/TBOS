29ff5bbc827719c9e4c344e23c108789dacc754b
Graham Hukill <ghukill@gmail.com>
2021-03-14 07:05:43 +0100

    lcd POC

--- embedded/main.py
+++ embedded/main.py
@@ -8,8 +8,14 @@ import time
 import pyb
 
 from embedded.debug import repl_ping
+from embedded.lcd import HD44780
 from embedded.resistance_motor import read_position_sensor, goto_level, rm_status
 from embedded.rpm_sensor import get_rpm
+
+# init LCD screen
+# NOTE: this may not work, as any command will wipe the screen
+lcd = HD44780()
+lcd.PINS = ["Y1", "Y2", "Y3", "Y4", "Y5", "Y6"]
 
 
 def status(lower_bound, upper_bound):
@@ -22,5 +28,19 @@ def status(lower_bound, upper_bound):
     rm_reading = rm_status(lower_bound, upper_bound)
     rpm_reading = get_rpm(print_results=False)
     response = {"rm": rm_reading, "rpm": rpm_reading, "elapsed": time.time() - t0}
+
+    # write to LCD
+    try:
+        lcd.init()
+        l1 = "lev:%s, cur:%s" % (str(response["rm"]["level"]), str(response["rm"]["current"]))
+        l2 = "rpm:%s" % (str(response["rpm"]["rpm"]))
+        lcd.write(
+            [l1, l2],
+            clear=True,
+        )
+    except Exception as e:
+        lcd.write(["ERROR", str(e)])
+
+    # print and return
     print(json.dumps(response))
     return response
--- flash_pyboard.sh
+++ flash_pyboard.sh
@@ -5,4 +5,5 @@ rshell cp ./embedded/resistance_motor.py /flash/embedded/resistance_motor.py
 rshell cp ./embedded/rpm_sensor.py /flash/embedded/rpm_sensor.py
 rshell cp ./embedded/inputs.py /flash/embedded/inputs.py
 rshell cp ./embedded/debug.py /flash/embedded/debug.py
+rshell cp ./embedded/lcd.py /flash/embedded/lcd.py
 echo "finis!"
