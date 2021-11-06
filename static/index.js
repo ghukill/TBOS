cf6661d4b4200f9dae81880a7e1a1ec96892c323
Graham Hukill <ghukill@gmail.com>
2021-11-06 16:57:17 +0100

    stylings

--- static/index.js
+++ static/index.js
@@ -1,17 +1,22 @@
-var data = {a: "sup boss."}
-var vm = new Vue({
-    el: '#vm',
-    data: data,
-    delimiters: ['[[', ']]'],
-    methods: {
-        apiPing: function () {
-            axios.get('http://localhost:5000/api/debug/ping')
-                .then(response => {
-                    const ping = response;
-                    alert(response.data);
-                })
-                .catch(error => console.error(error));
-        }
-    }
-})
+/*
+Base JS
+ */
 
+console.log('base JS loaded')
+
+export function makePrettyTime (inputSeconds) {
+  /**
+  * [Pretty format seconds into HH:MM:SS]
+  * @param  {[int]} inputSeconds [input seconds]
+  */
+
+  var secNum = parseInt(inputSeconds, 10)
+  var hours = Math.floor(secNum / 3600)
+  var minutes = Math.floor((secNum - (hours * 3600)) / 60)
+  var seconds = secNum - (hours * 3600) - (minutes * 60)
+
+  if (hours < 10) { hours = '0' + hours }
+  if (minutes < 10) { minutes = '0' + minutes }
+  if (seconds < 10) { seconds = '0' + seconds }
+  return hours + ':' + minutes + ':' + seconds
+}
